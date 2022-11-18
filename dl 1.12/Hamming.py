1fe6247814849e476541483c146f887904e71993
nuneung1 <gyugg21@gmail.com>
2022-11-18 16:02:07 +0700

    Update Hamming.py

--- dl 1.12/Hamming.py
+++ dl 1.12/Hamming.py
@@ -4,10 +4,8 @@ def main():
     text1 = input()
     text2 = input()
     val = 0
-    for i in range(len(min(text1, text2))): #จริงๆข้อความความยาวเท่ากัน ไม่ได้อ่าน inputspec ดีๆ ก็งี้
+    for i in range(len(text1)):
         if text2[i] != text1[i]:
             val += 1
-    diff = abs(len(text1) - len(text2))
-    val -= diff
     print(val)
 main()
