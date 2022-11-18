33f0b1d7916c9d4280e371c76914ad45ccda30a2
nuneung1 <gyugg21@gmail.com>
2022-11-18 19:00:13 +0700

    pair

---
+++ dl 18.11/RectangleArea.py
@@ -0,0 +1,16 @@
+"""rectangle area"""
+def main():
+    """rectangle area"""
+    rec1x2, rec1y2, width1, height1 = [int(x) for x in input().split()]
+    rec1x1 = rec1x2 + width1
+    rec1y1 = rec1y2 + height1
+    rec2x2, rec2y2, width2, height2 = [int(x) for x in input().split()]
+    rec2x1 = rec2x2 + width2
+    rec2y1 = rec2y2 + height2
+
+    width = max(0, min(rec1x1, rec2x1) - max(rec1x2, rec2x2))
+    height = max(0, min(rec1y1, rec2y1) - max(rec1y2, rec2y2))
+
+    print(width * height if width * height > 0 else "no overlapping")
+
+main()
